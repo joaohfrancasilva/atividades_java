e428460434adb90a1fe256efaf47f7b8681c3dac
joaohfrancasilva <joao.franca@maisunifacisa.com.br>
2026-05-06 07:55:29 -0300

    Update JogoDaVelha.java

--- JogoDaVelha.java
+++ JogoDaVelha.java
@@ -4,20 +4,20 @@ public class JogoDaVelha{
         Scanner input = new Scanner(System.in);
         String[][] jogo = {{"[ ]", "[ ]", "[ ]"}, {"[ ]", "[ ]", "[ ]"}, {"[ ]", "[ ]", "[ ]"}};
         boolean fim_do_jogo = false;
-        String vencedor = "";
+        boolean velha = false;
         do{
+            boolean jogou = true;
             int qnt_jogadas = 0;
+            int linha = -1, coluna = -1;;
             System.out.println("Jogo da velha: ");
             for (int i = 0; i < 3; i++){
                 if (jogo[i][0] == jogo[i][1] && jogo[i][0] == jogo[i][2] && jogo[i][0] != "[ ]"){
                     fim_do_jogo = true;
-                    vencedor = jogo[i][0];
                 }
                 for (int j = 0; j < 3; j++){
                         System.out.print(jogo[i][j]);
                         if (jogo[0][j] == jogo[1][j] && jogo[0][j] == jogo[2][j] && jogo[0][j] != "[ ]"){
                             fim_do_jogo = true;
-                            vencedor = jogo[0][j];
                         }
                         if (jogo[i][j] != "[ ]"){
                             qnt_jogadas += 1;
@@ -27,19 +27,28 @@ public class JogoDaVelha{
             }
             if (jogo[1][1] != "[ ]" && ((jogo[1][1] == jogo[0][0] && jogo[1][1] == jogo[2][2]) || (jogo[1][1] == jogo[0][2] && jogo[1][1] == jogo[2][0]))){
                 fim_do_jogo = true;
-                vencedor = jogo[1][1];
             }
             if(qnt_jogadas == 9){
-                vencedor = "velha";
                 fim_do_jogo = true;
+                velha = true;
             }
             if(fim_do_jogo == true){break;}
-            System.out.print("Escolha aonde você vai jogar: \nLinha: ");
-            int linha = input.nextInt();
-            System.out.print("Coluna: ");
-            int coluna = input.nextInt();
-            jogo[linha][coluna] = "[X]";
-            if(jogo[1][1] == "[X]"){
+            System.out.println("Escolha aonde você vai jogar:");
+            while(linha > 2 || linha < 0){
+                System.out.print("Linha (0 até 2): ");
+                linha = input.nextInt();
+            }
+            while(coluna > 2 || coluna < 0){
+                System.out.print("Coluna (0 até 2): ");
+                coluna = input.nextInt();
+            }
+            if(jogo[linha][coluna] == "[ ]"){
+                jogo[linha][coluna] = "[X]";
+            }
+            else{
+                jogou = false;
+            }
+            if(jogo[1][1] == "[X]"  && jogou == true){
                 jogo[0][0] = "[O]";
                 if((jogo[2][2] == "[X]" && jogo[2][0] == "[ ]") || jogo[2][0] == "[X]"){
                     jogo[0][2] = "[O]";
@@ -48,7 +57,7 @@ public class JogoDaVelha{
                         if(jogo[1][0] == "[X]"){
                             jogo[1][2] = "[O]";
                         }
-                        else{
+                        else if (qnt_jogadas != 4){
                             jogo[1][0] = "[O]";
                         }
                     }
@@ -56,7 +65,7 @@ public class JogoDaVelha{
                         jogo[0][1] = "[O]";
                     }
                 }
-                else if(jogo[0][2] == "[X]"){
+                else if(jogo[0][2] == "[X]" && jogo[2][1] == "[ ]"){
                     jogo[2][0] = "[O]";
                     if(jogo[1][0] == "[X]"){
                         jogo[1][2] = "[O]";
@@ -78,11 +87,11 @@ public class JogoDaVelha{
                         if (jogo[1][0] == "[X]"){
                             jogo[1][2] = "[O]";
                         }
-                        else{
+                        else if (qnt_jogadas != 4){
                             jogo[1][0] = "[O]";
                         }
                     }
-                    else{
+                    else if (qnt_jogadas != 2){
                         jogo[0][2] = "[O]";
                     }
                 }
@@ -101,6 +110,54 @@ public class JogoDaVelha{
                         jogo[2][0] = "[O]";
                     }
                 }
+                else if (jogo[0][1] == "[X]"){
+                    jogo[2][1] = "[O]";
+                    if (jogo[0][2] == "[X]"){
+                        jogo[2][0] = "[O]";
+                        if (jogo[1][0] == "[X]"){
+                            jogo[1][2] = "[O]";
+                        }
+                        else if (qnt_jogadas != 4){
+                            jogo[1][0] = "[O]";
+                        }
+                    }
+                    else if (jogo[1][2] == "[X]"){
+                        jogo[1][0] = "[O]";
+                        if (jogo[2][0] == "[X]"){
+                            jogo[0][2] = "[O]";
+                        }
+                        else if(qnt_jogadas != 4){
+                            jogo[2][0] = "[O]";
+                        }
+                    }
+                    else if (jogo[2][2] == "[X]"){
+                        jogo[1][0] = "[O]";
+                        if (jogo[2][0] == "[X]"){
+                            jogo[0][2] = "[O]";
+                        }
+                        else if(qnt_jogadas != 4){
+                            jogo[2][0] = "[O]";
+                        }
+                    }
+                    else if (jogo[1][0] == "[X]"){
+                        jogo[1][2] = "[O]";
+                        if (jogo[0][2] == "[X]"){
+                            jogo[2][0] = "[O]";
+                        }
+                        else{
+                            jogo[0][2] = "[O]";
+                        }
+                    }
+                    else{
+                        jogo[0][2] = "[O]";
+                        if(jogo[1][0] == "[X]"){
+                            jogo[1][2] = "[O]";
+                        }
+                        else{
+                            jogo[1][0] = "[O]";
+                        }
+                    }
+                }
                 else if (jogo[1][0] == "[X]"){
                     jogo[1][2] = "[O]";
                     if (jogo[2][0] == "[X]"){
@@ -112,7 +169,7 @@ public class JogoDaVelha{
                             jogo[0][1] = "[O]";
                         }
                     }
-                    if (jogo[0][2] == "[X]"){
+                    else if (jogo[0][2] == "[X]"){
                         jogo[2][0] = "[O]";
                         if (jogo[2][1] == "[X]"){
                             jogo[0][1] = "[O]";
@@ -121,7 +178,7 @@ public class JogoDaVelha{
                             jogo[2][1] = "[O]";
                         }
                     }
-                    if (jogo[2][1] == "[X]"){
+                    else if (jogo[2][1] == "[X]"){
                         jogo[0][1] = "[O]";
                         if (jogo[0][2] == "[X]"){
                             jogo[2][0] = "[O]";
@@ -130,7 +187,7 @@ public class JogoDaVelha{
                             jogo[0][2] = "[O]";
                         }
                     }
-                    if (jogo[2][2] == "[X]"){
+                    else if (jogo[2][2] == "[X]"){
                         jogo[0][1] = "[O]";
                         if (jogo[0][2] == "[X]"){
                             jogo[2][0] = "O";
@@ -143,8 +200,8 @@ public class JogoDaVelha{
             }
         }while (fim_do_jogo != true);
         input.close();
-        if (vencedor != "velha"){
-        System.out.printf("O jogador que usava %s venceu!", vencedor);}
+        if (velha == false){
+        System.out.printf("O jogador que usava [O] venceu!");}
         else{
             System.out.println("Deu velha...");
         }
